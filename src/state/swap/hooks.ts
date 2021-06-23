db1aaf8d1e1d6737ba6f4d3d421c4255635ddf0b
pixiuswap <83927033+pixiuswap@users.noreply.github.com>
2021-06-23 20:54:04 +0300

    Update hooks.ts

--- src/state/swap/hooks.ts
+++ src/state/swap/hooks.ts
@@ -87,9 +87,9 @@ export function tryParseAmount(value?: string, currency?: Currency): CurrencyAmo
 }
 
 const BAD_RECIPIENT_ADDRESSES: string[] = [
-  '0xBCfCcbde45cE874adCB698cC183deBcF17952812', // v2 factory
+  '0x091b13a0a018dEdD4fc566371ed496d4C1ce317E', // v2 factory
   '0xf164fC0Ec4E93095b804a4795bBe1e041497b92a', // v2 router 01
-  '0x05fF2B0DB69458A0750badebc4f9e13aDd608C7F', // v2 router 02
+  '0x091b13a0a018dEdD4fc566371ed496d4C1ce317E', // v2 router 02
 ]
 
 /**
